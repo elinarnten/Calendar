2178935f9b70139f4af009e33972b4b445f8e3d2
elinarnten <elin.arnten@hotmail.com>
2021-12-08 11:55:37 +0100

    get calendar api

--- calendar.js
+++ calendar.js
@@ -1,4 +1,14 @@
 
 // const weekday = ["Måndag","Tisdag","Onsdag","Torsdag","Fredag","Lördag","Söndag"];
 
+window.onload = ('load', main);
 
+function main() {
+    getCalendar();
+}
+
+async function getCalendar() {
+    fetch('http://sholiday.faboul.se/dagar/v2.1/2015')
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
