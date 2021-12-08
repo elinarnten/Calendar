acd4003087acbc19c7f11a2d52709574051714e1
elinarnten <elin.arnten@hotmail.com>
2021-12-08 14:45:03 +0100

    present the year month and day

--- calendar.js
+++ calendar.js
@@ -8,7 +8,21 @@ function main() {
 }
 
 async function getCalendar() {
-    fetch('http://sholiday.faboul.se/dagar/v2.1/2015')
+    const currentMonth = new Date().getMonth();
+    const currentYear = new Date().getFullYear();
+    const currentDay = new Date().getDay();
+    
+    const promises = [];
+
+    for (let month = currentMonth; month > currentMonth -5; month-- ) {
+        const promise = fetch(`http://sholiday.faboul.se/dagar/v2.1/${currentYear}/${currentMonth}/${currentDay}`);
+        promises.push(promise);
+    }
+    const responses = await Promise.all(promises);
+    console.log(responses);
+    /*
+    fetch(`http://sholiday.faboul.se/dagar/v2.1/${currentYear}/${currentMonth}/${currentDay}`)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => console.log(data));*/
+
 }
