6a8113091455b0ed1fa4e5bf3598fd618f2f8bff
Jannie87 <jannie.backman@gmail.com>
2021-12-09 07:52:13 +0100

    added css to todoelement

--- todos.js
+++ todos.js
@@ -1,49 +1,51 @@
-window.addEventListener('load', main);
-let todos = ['Köpa julklappar', 'Klä granen', 'julpynta'];
+// window.addEventListener('load', main);
+// let todos = ['Köpa julklappar', 'Klä granen', 'julpynta'];
 
-function main() {
-    renderTodos();
-    const form = document.querySelector('form');
-    form.addEventListener('submit', addTodo);
-}
 
-function addTodo(event) {
-    event.preventDefault();
-    const form = event.target;
-    const input = form.querySelector('input');
-    if (input.value) {
-        todos.push(input.value);
-        input.value = "";
-        renderTodos();
-    }
-}
+// function main() {
+//     renderTodos();
+//     const form = document.querySelector('form');
+//     form.addEventListener('submit', addTodo);
+// }
 
-function renderTodos() {
-    const ul = document.querySelector('ul');
-    ul.innerHTML ="";
-    for(const todo of todos) {
-        const li = createTodoElement(todo);
-        ul.appendChild(li); //utöka listan
-    }
-}
+// function addTodo(event) {
+//     event.preventDefault();
+//     const form = event.target;
+//     const input = form.querySelector('input');
+//     if (input.value) {
+//         todos.push(input.value);
+//         input.value = "";
+//         renderTodos();
+//     }
+// }
 
-function createTodoElement(todo) {
-    const li = document.createElement('li');
-    li.innerHTML = todo;
-    li.id=todo;
-    li.addEventListener('click', removeTodo);
-    return li;
-}
+// function renderTodos() {
+//     const ul = document.querySelector('ul');
+//     ul.innerHTML ="";
+//     for(const todo of todos) {
+//         const li = createTodoElement(todo);
+//         ul.appendChild(li); //utöka listan
+//     }
+// }
 
-function removeTodo(event) {
-    const todoToDelete= event.target.id;
+// function createTodoElement(todo) {
+//     const li = document.createElement('li');
+//     li.innerHTML = todo;
+//     li.id=todo;
+//     li.addEventListener('click', removeTodo);
+//     return li;
+    
+// }
 
-    const updateTodoList = [];
-    for(const todo of todos) {
-        if(todo!== todoToDelete) {
-            updateTodoList.push(todo);
-        }
-    }
-    todos = updateTodoList;
-    renderTodos();
-}
+// function removeTodo(event) {
+//     const todoToDelete= event.target.id;
+
+//     const updateTodoList = [];
+//     for(const todo of todos) {
+//         if(todo!== todoToDelete) {
+//             updateTodoList.push(todo);
+//         }
+//     }
+//     todos = updateTodoList;
+//     renderTodos();
+// }
