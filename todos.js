81ffca8bfb3040eab52be985505b88a3cb05dfe5
ROSANNA PISTONE <rosanna@pistone.se>
2021-12-16 13:56:37 +0100

    Add inputedit grej
    Co-authored-by: Jannie Bäckman Kuurne <Jannie87@users.noreply.github.com>
    Co-authored-by: elinarnten <elinarnten@users.noreply.github.com>

--- todos.js
+++ todos.js
@@ -1,94 +1,90 @@
-let inputElement = document.querySelector('input');
-let formElement = document.querySelector('form');
-let listElement = document.querySelector('ul');
-let totalTasksElement = document.querySelector('#total-tasks');
+let inputElement = document.querySelector("input");
+let formElement = document.querySelector("form");
+let listElement = document.querySelector("ul");
+let totalTasksElement = document.querySelector("#total-tasks");
 
-let tasklist = [
-  'klä julgran',
-  'julpynta'
-];
-
+let tasklist = ["klä julgran", "julpynta"];
+console.log(tasklist);
 
 //Add Item function
-function addTask () {
+function addTask() {
   if (inputElement.value) {
-    tasklist.push(inputElement.value);    
+    tasklist.push(inputElement.value);
     renderList();
   }
 }
 
-function editItem (event, span, item, deleteElement, editElement) {
-let editInput = document.createElement('input');
-editInput.value = item;
-span.innerHTML = "";
-span.appendChild(editInput);
-deleteElement.innerHTML = '<i class="fas fa-times"></i>';
-let saveEditIcon = document.createElement('i');
-saveEditIcon.innerHTML = '<i id="saveEdit" class="fas fa-check"></i>';
-editElement.innerHTML = '';
-editElement.appendChild(saveEditIcon)
-// Om editElement klickas ska editInput.value ska sparas och visas som inneHTML för span. 
-editElement.addEventListener('submit', saveEdit(event, span, item, saveEditIcon, editInput));
+function showTodoDate(item, span) {
+  let inputDate = document.getElementById("inputDate");
+  if (inputDate.value) {
+    tasklist.push(inputDate.value);
+  }
+}
 
+function editItem(event, span, item, deleteElement, editElement) {
+  console.log('gjghxfdzcd');
+  let editInput = document.createElement("input");
+  editInput.value = item;
+  span.innerHTML = "";
+  span.appendChild(editInput);
+  deleteElement.innerHTML = '<i class="fas fa-times"></i>';
+  let saveEditIcon = document.createElement("i");
+  saveEditIcon.innerHTML = '<i id="saveEdit" class="fas fa-check"></i>';
+  editElement.innerHTML = "";
+  editElement.appendChild(saveEditIcon);
+  // Om editElement klickas ska editInput.value ska sparas och visas som inneHTML för span.
+  editElement.onclick = (event) =>
+    saveEdit(event, span, item, saveEditIcon, editInput);
 }
 function saveEdit(event, span, item, saveEditIcon, editInput) {
-
-console.log(item)
+  console.log(item);
 }
-
-
 
 //Delete Item function
 function deleteItem(event) {
-  let item= event.target.parentElement.previousElementSibling.innerHTML;
+  let item = event.target.parentElement.previousElementSibling.innerHTML;
   let index = tasklist.indexOf(item);
-  if(index !== -1){
+  if (index !== -1) {
     tasklist.splice(index, 1);
   }
- renderList();
+  renderList();
 }
 
 function renderList() {
-  listElement.innerHTML='';
-  tasklist.forEach(function(item) {
-    let newItem = document.createElement('li');
+  listElement.innerHTML = "";
+  tasklist.forEach(function (item) {
+    let newItem = document.createElement("li");
     //Add a new span
-    let span = document.createElement('span');
+    let span = document.createElement("span");
     span.innerHTML = item;
     newItem.appendChild(span);
 
     //Add delete button
-    let deleteElement = document.createElement('i');
-    deleteElement.classList.add('delete');
+    let deleteElement = document.createElement("i");
+    deleteElement.classList.add("delete");
     deleteElement.innerHTML = '<i class="fas fa-trash"></i>';
     newItem.appendChild(deleteElement);
-    deleteElement.addEventListener('click', (event) => deleteItem(event));
-    
+    deleteElement.addEventListener("click", (event) => deleteItem(event));
+
     // Add edit button
-    let editElement = document.createElement('i');
-    editElement.classList.add('edit');
+    let editElement = document.createElement("i");
+    editElement.classList.add("edit");
     editElement.innerHTML = '<i id="editIcon" class="fas fa-edit"></i>';
     newItem.appendChild(editElement);
-    editElement.addEventListener('click', (event) => editItem(event, span, item, deleteElement, editElement));
-
+    editElement.onclick = ((event) =>
+      editItem(event, span, item, deleteElement, editElement)
+    );
+    showTodoDate(item, span);
 
     // add li to ul
     listElement.appendChild(newItem);
   });
   totalTasksElement.innerHTML = tasklist.length;
-  inputElement.value='';
+  inputElement.value = "";
 }
 renderList();
 
-
-
-
-
-formElement.addEventListener('submit', function(event) {
+formElement.addEventListener("submit", function (event) {
   event.preventDefault();
-addTask();
-
+  addTask();
 });
-
-
-
