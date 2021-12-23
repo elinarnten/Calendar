74a840e1c978e10f001743734c63091f68098f2f
elinarnten <elin.arnten@hotmail.com>
2021-12-23 15:29:49 +0100

    finally fixed the holiday

--- Script/calendar.js
+++ Script/calendar.js
@@ -1,9 +1,10 @@
 const date = new Date();
+let holidays = [];
 window.onload = ('load', main);
 
 function main() {
     renderSelectedMonth();
-    //getHolidays();
+    getHolidays();
     addEventListener();
 }
 
@@ -17,14 +18,25 @@ function renderSelectedMonth() {
     resetCalendar();
     uppdateMonthName(date);
     syncDatesAndWeekdays(date, dateContainer);
-    getCalendar(date, dateContainer);
+    renderCalendar(date, dateContainer);
 }
 
 /* async function getHolidays() {
-    fetch('http://sholiday.faboul.se/dagar/v2.1/2015')
+    fetch('http://sholiday.faboul.se/dagar/v2.1/2015/01')
     .then(res => res.json())
     .then(data => console.log(data));
 } */
+async function getHolidays() { 
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const response = await fetch(`https://sholiday.faboul.se/dagar/v2.1/${year}/${month}`);
+    const data = await response.json();
+    
+    holidays = data.dagar.filter((day) => day.helgdag);
+    renderSelectedMonth();
+    console.log([holidays])
+} 
+  
 function syncDatesAndWeekdays (date, dateContainer) {
     const weekdays = new Date(date.getFullYear(), date.getMonth(), 0).getDay();
     for (let i = 0; i < weekdays; i++) {
@@ -33,11 +45,11 @@ function syncDatesAndWeekdays (date, dateContainer) {
     }
 }
 
-function getCalendar(date, dateContainer) {
+function renderCalendar(date, dateContainer) {
     const daysOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
     const month = (date.getMonth() + 1);
     const year = (date.getFullYear());
-    const day = (date.getDate());
+    /* const day = (date.getDate()); */
     for (let i = 1; i <= daysOfMonth; i++) {
         const dayContainer = document.createElement('div');
         dayContainer.setAttribute("id", "day-container");
@@ -64,24 +76,31 @@ function getCalendar(date, dateContainer) {
             numberOfItemsPerDay.innerText = todosForCurrentDay.length;
             dayContainer.onclick = () => showInTodoList(item, todosForCurrentDay);  
           }   
+        }); 
           //console.log(todosForCurrentDay)
-    }); 
-}  
+
+          let holidayPerMonth = document.createElement("p");
+          holidayPerMonth.setAttribute("id", "holiday");
+          holidays.filter((day) => {
+          if (day.datum === searchDate){
+              dayContainer.appendChild(holidayPerMonth)
+              holidays.push(day);
+              holidayPerMonth.innerHTML = day.helgdag;
+        }
+    })
+    }  
 }
 
 function showInTodoList(item, todosForCurrentDay) {
     todosForCurrentDay.filter((item) => {
     if (todosForCurrentDay.length < 0) {
-       /* let newItem = document.getElementById('new-item'); 
+       let newItem = document.getElementById('new-item'); 
        newItem.innerHTML = 'hej';
-       listElement.appendChild(newItem);*/
+       listElement.appendChild(newItem);
     }
     console.log(todosForCurrentDay.length)
 })
-
 }
-
-
     // Skapa badge utifrån filtreringen
     // Filterera ut alla todos för den aktuella dagen i loopen
 
@@ -92,11 +111,13 @@ function resetCalendar() {
 function prevMonth() {
     date.setMonth(date.getMonth() - 1);
     renderSelectedMonth();
+    getHolidays();
 }
 
 function nextMonth(){
     date.setMonth(date.getMonth() + 1);
     renderSelectedMonth();
+    getHolidays();
 }
 
 function uppdateMonthName(date) {
